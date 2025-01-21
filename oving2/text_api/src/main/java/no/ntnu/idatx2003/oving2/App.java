e9879248fdac67e6e1226eb521ee216dda94d1b0
dennismoe0 <dennismoe0@gmail.com>
2025-01-21 20:31:47 +0100

    Configured a basic maven structure that uses JAVA 23, JUNIT 5 and basic maven structure.

---
+++ oving2/text_api/src/main/java/no/ntnu/idatx2003/oving2/App.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatx2003.oving2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ oving2/text_api/src/test/java/no/ntnu/idatx2003/oving2/AppTest.java
@@ -0,0 +1,18 @@
+package no.ntnu.idatx2003.oving2;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
